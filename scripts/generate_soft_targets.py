fed062290a157347b00294bbf91bd77e87785f78
JingWei <135567358+jingweii97@users.noreply.github.com>
2026-06-27 17:38:40 +0800

    - fix import

--- scripts/generate_soft_targets.py
+++ scripts/generate_soft_targets.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import torch
+import numpy as np
 
 # Add repository root to python path to allow importing packages
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
